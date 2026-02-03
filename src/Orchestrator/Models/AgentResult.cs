ff5b681d03b33ff06777d2f94ae4103d9ee5e18a
CloudNexus <abhighnagiri@gmail.com>
2026-02-03 19:06:05 +0530

    Create AgentResult.cs

---
+++ src/Orchestrator/Models/AgentResult.cs
@@ -0,0 +1,9 @@
+namespace  Orchestrator.Models;
+
+public  class  AgentResult
+{
+        public  string  WorkflowId  {  get;  set;  }  =  default!;
+        public  string  TaskId  {  get;  set;  }  =  default!;
+        public  string  AgentType  {  get;  set;  }  =  default!;
+        public  string  Result  {  get;  set;  }  =  default!;
+}
