c5b4406e7258bf3673ed50336ba502b86481fbd2
CloudNexus <abhighnagiri@gmail.com>
2026-02-03 19:05:17 +0530

    Create QueueNames.cs

---
+++ src/Shared/QueueNames.cs
@@ -0,0 +1,9 @@
+namespace  Shared;
+
+public  static  class  QueueNames
+{
+        public  const  string  PlannerTasks  =  "planner-tasks";
+        public  const  string  ResearchTasks  =  "research-tasks";
+        public  const  string  SummarizerTasks  =  "summarizer-tasks";
+        public  const  string  ExecutorTasks  =  "executor-tasks";
+}
